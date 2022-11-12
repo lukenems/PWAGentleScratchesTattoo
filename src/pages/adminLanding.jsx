2925b53114fe8a2062042741a17fc8d7da6d3688
Luke Nemy <lukenemy7@gmail.com>
2022-11-11 17:30:08 -1000

    nylas agenda component working!

--- src/pages/adminLanding.jsx
+++ src/pages/adminLanding.jsx
@@ -3,15 +3,12 @@ import SchedulerEditor from '../protected/SchedulerEditor';
 
 const AdminLanding = () => {
   const today = new Date();
-  const selected_date = new Date(
-  )
-
   return (
     <div className="bg-gradient-to-r from-green-500 via-transparent to-green-500 h-full">
       <div className="flex flex-col flex-between justify-around sm:flex-row pt-8">
         <div className="flex flex-col w-1/3 items-center py-2 px-2 sm:px-2 mx-3 bg-purple-900 border-2 rounded-l-lg border-white">
-          <h1>First</h1>
-          <h1>Full</h1>
+          <h1>Payouts Last 30 days</h1>
+          <div className="px-3 bg-gray-500"></div>
         </div>
         <div className="flex flex-col w-1/3 items-center text-center py-2 px-2 sm:px-2 mx-3 bg-purple-900 border-2 rounded-2xl border-white">
           <h1>Upcoming Bookings</h1>
@@ -19,7 +16,7 @@ const AdminLanding = () => {
             <nylas-agenda
               header_type="full"
               allow_date_change={true}
-              selected_date={selected_date}
+              selected_date={today}
               condensed_view={true}
               id={import.meta.env.VITE_NYLAS_CALENDAR_ID}
               allowed_dates
@@ -30,7 +27,7 @@ const AdminLanding = () => {
         </div>
         <div className="flex flex-col w-1/3 items-center text-center py-2 px-2 sm:px-2 mx-3 bg-purple-900 border-2 rounded-r-lg border-white">
           <h1>Recent Custom Requests</h1>
-          <h1>Full</h1>
+          <div className="px-3 bg-gray-500"></div>
         </div>
       </div>
       <div className="flex flex-col min-h-full items-center py-12 px-4 sm:px-6 lg:px-8">
