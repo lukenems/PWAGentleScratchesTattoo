270c9226262f50a8252b9fbf8893726de8ce8ab4
Luke Nemy <lukenemy7@gmail.com>
2022-11-26 13:19:04 -1000

    fix for uploading of flash urls to realtime

--- src/hooks/useUploadFlashUrls.jsx
+++ src/hooks/useUploadFlashUrls.jsx
@@ -1,11 +1,14 @@
 import { db } from '../firebase';
-import { ref, update } from 'firebase/database';
+import { ref, update, child, push } from 'firebase/database';
 
 const useUploadFlashUrls = async (urls) => {
   console.log(urls)
-  const dbRef = ref(db, 'Flash_Urls/')
+  // const dbRef = ref(db, 'Flash_Urls/')
+  const newPostKey = push(child(ref(db), 'Flash_Urls')).key;
+  const updates = {};
+  updates['Flash_Urls/' + newPostKey] = urls;
   try {
-    update(dbRef, {urls})
+    update(ref(db), updates)
       .then(() => {
         console.log('Uploaded successfully');
       })
