a1751832e7ebd743ccd6fd8a3da6a01dc93785d4
Luke Nemy <lukenemy7@gmail.com>
2022-12-05 13:21:42 -1000

    added logo; fixed landing navigation

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,14 @@
 import CalendarScroll from "../components/CalendarScroll";
 import FlashScroll from "../components/FlashScroll";
+import './home.css';
 
 const Home = () => {
 
   return (
-    <>
+    <div className="flex flex-col sm:flex-row min-w-full min-h-screen">
       <FlashScroll />
       <CalendarScroll />
-    </>
+    </div>
   )
 }
 
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,14 +1,13 @@
-import FlashScroll from '../components/FlashScroll';
-import CalendarScroll from '../components/CalendarScroll';
 import { useEffect, useState } from 'preact/hooks';
-import { setLocation } from 'wouter/preact'
+import { useLocation } from 'wouter/preact'
+import landingLogo from '../assets/LandingLogo.png';
+import redLogo from '../assets/RedLandingLogo.png';
 
 const Landing = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [buttonLoad, setButtonLoad] = useState(false);
-  
-  // add login in here for banner to cover screen until calendar and flash are loaded
-  // maybe even just a simple spinner
+  const [location, setLocation] = useLocation();
+
   useEffect(() => {
     setTimeout(() => {
       setButtonLoad(true);
@@ -19,35 +18,28 @@ const Landing = () => {
     e.preventDefault();
     setIsLoaded(true);
     setTimeout(() => {
-      // const landingCover = document.getElementById('landing-cover');
-      // landingCover.style.display = 'none';
-      // localStorage.setItem('hasLanded', true)
       setLocation('/home');
     }, 2500);
   }
 
   return (
     <div className="flex flex-col sm:flex-row min-w-full min-h-screen">
-        {!localStorage.getItem('hasLanded') &&
-          <div id="landing-cover">
-            <img src="" alt="Strawberry with sexy librarian glasses" />
-            <h3>Welcome to</h3>
-            <h1>Gentle Scratchess Tattoo</h1>
-            <div className='m-2 p-3 h-10 w-15'>
-              { buttonLoad && 
-                <button 
-                onClick={enterClick}
-                className='text-gray-200 animate-bounce'
-                id='landing-btn'
-                >
-                  {!isLoaded ? 'Enter' : 'Come on in!'}
-                </button>
-              }
-            </div>
-          </div>
-        }
-        <FlashScroll />
-        <CalendarScroll />
+      <div id="landing-cover">
+        <div id='logo' className='mb-12'/>
+        <h3>Welcome to</h3>
+        <h1>Gentle Scratches Tattoo</h1>
+        <div className='m-2 p-3 h-10 w-15'>
+          { buttonLoad && 
+            <button 
+            onClick={enterClick}
+            className='text-gray-200 animate-bounce'
+            id='landing-btn'
+            >
+              {!isLoaded ? 'Enter' : 'Come on in!'}
+            </button>
+          }
+        </div>
+      </div>
     </div>
   );
 };
