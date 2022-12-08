cee099a29bb4613b20f658faa20969a60f1e1896
Luke Nemy <lukenemy7@gmail.com>
2022-12-07 17:15:05 -1000

    retrieving custom requests, and displaying imgs in modal

---
+++ src/components/ViewCustomImages.jsx
@@ -0,0 +1,27 @@
+const ViewCustomImages = (props) => {
+  return (
+    <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div class="sm:flex sm:items-start">
+            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">Urls:</h3>
+              <div class="mt-2">
+                {props.urlList.map(url => {
+                  return (
+                      // <p class="text-sm text-gray-500">{url}</p>
+                      <img src={url} alt="" />
+                    )
+                })
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button onClick={props.cancelModal} type="button" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Close</button>
+        </div>
+      </div>
+  )
+}
+
+export default ViewCustomImages;
--- src/protected/RecentCustomRequests.jsx
+++ src/protected/RecentCustomRequests.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "preact/hooks";
+import ViewCustomImages from "../components/ViewCustomImages";
 import useCustomRequests from "../hooks/useCustomRequests";
 
 const RecentCustomRequests = () => {
   const [recentReqs, setRecentReqs] = useState([]);
+  const [loadModal, setLoadModal] = useState(false);
 
   useEffect(async () => {
     const reqList = await useCustomRequests();
     setRecentReqs(reqList);
   },[])
+
+  const displayModal = () => {
+    setLoadModal(!loadModal);
+  }
 
   return (
     <>
@@ -17,7 +23,9 @@ const RecentCustomRequests = () => {
           <h3>{client.name}</h3>
           <h3>{client.desc}</h3>
           <h3>{client.email}</h3>
-          <h3>{client.insta}</h3> 
+          <h3>{client.insta}</h3>
+          <button onClick={displayModal}>See Images</button>
+          {loadModal && <ViewCustomImages urlList={client.urls} cancelModal={displayModal}/>}
         </>
         )
       })
